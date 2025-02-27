0eb4f3790fbed233036591c9a5e55918196dd4c5
Mohammed Saleh <104917244+Mdsaleh99@users.noreply.github.com>
2025-02-27 23:27:45 +0530

    Fix: update URL for debouncing and throttling reference in comments

--- 02-Phase/JS-fast-phase(22feb)/05_session/machine_coding/02.js
+++ 02-Phase/JS-fast-phase(22feb)/05_session/machine_coding/02.js
@@ -17,7 +17,7 @@ const pataNahi = (fn, delay) => {
 // the above code is a concept of Throttling
 // e.g: in coding platform the run button delayed for 30 second when we click run button for 2nd time
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
-// www.geeksforgeeks.org/difference-between-debouncing-and-throttling/
+// https://www.geeksforgeeks.org/difference-between-debouncing-and-throttling/
 
 // VERY IMPORTANT NOTE TO UNDERSTAND JAVASCRIPT ==> konse memory me data jara hai and oh daigram (event loop) yaad rahkna hai because functions kis kis queue me jara hai aur kaise execution hora hai
 
